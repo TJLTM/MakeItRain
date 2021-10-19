7e990d325405a81ff261822a59becddd42307242
David Gonzalez <davidsanchez23@icloud.com>
2021-10-19 15:53:30 -0700

    Initial UI Commit Staging

---
+++ Code/MakeItRain-Ui/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatSidenavModule} from '@angular/material/sidenav';
+
+
+import { MirComponent } from './mir/mir.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MirComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Code/MakeItRain-Ui/src/app/mir/mir.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mir',
+  templateUrl: './mir.component.html',
+  styleUrls: ['./mir.component.scss']
+})
+export class MirComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
