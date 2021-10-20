4990bb1a372d8f57e6e393ebe7815643ad3c7a9f
David Gonzalez <davidsanchez23@icloud.com>
2021-10-19 21:00:26 -0700

    settings staging

--- Code/MakeItRain-Ui/src/app/app.module.ts
+++ Code/MakeItRain-Ui/src/app/app.module.ts
@@ -6,22 +6,32 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTabsModule} from '@angular/material/tabs';
 
 
 import { MirComponent } from './mir/mir.component';
+import { ZonesComponent } from './mir/selections/zones/zones.component';
+import { SettingsComponent } from './mir/selections/settings/settings.component';
+import { ZoneInfoComponent } from './mir/selections/zones/zone-info/zone-info.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    MirComponent
+    MirComponent,
+    ZonesComponent,
+    SettingsComponent,
+    ZoneInfoComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatToolbarModule,
     MatIconModule,
-    MatSidenavModule
+    MatButtonModule,
+    MatSidenavModule,
+    MatTabsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Code/MakeItRain-Ui/src/app/mir/mir.component.ts
+++ Code/MakeItRain-Ui/src/app/mir/mir.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mir.component.scss']
 })
 export class MirComponent implements OnInit {
-
+  public display_content: string = "zones";
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+
+  setView(value: string): void{
+    this.display_content = value;
   }
 
 }
