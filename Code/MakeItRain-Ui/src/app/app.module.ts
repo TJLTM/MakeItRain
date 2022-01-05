5ea4de2dcdfdaed049ee87d31adcfe63bf8dcf15
David Gonzalez <davidsanchez23@icloud.com>
2022-01-04 21:33:37 -0800

    Settings Card Staging

--- Code/MakeItRain-Ui/src/app/app.module.ts
+++ Code/MakeItRain-Ui/src/app/app.module.ts
@@ -3,13 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatButtonModule} from '@angular/material/button';
-import {MatTabsModule} from '@angular/material/tabs';
 
+// Supporting Modules
+import { NgxMasonryModule } from 'ngx-masonry';
 
+// Materials Components
+import { MatToolbarModule} from '@angular/material/toolbar';
+import { MatIconModule} from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatSidenavModule} from '@angular/material/sidenav';
+import { MatButtonModule} from '@angular/material/button';
+import { MatTabsModule} from '@angular/material/tabs';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatTableModule } from '@angular/material/table';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDialogModule } from '@angular/material/dialog';
+
+//Make it Rain Components
 import { MirComponent } from './mir/mir.component';
 import { ZonesComponent } from './mir/selections/zones/zones.component';
 import { SettingsComponent } from './mir/selections/settings/settings.component';
@@ -27,11 +38,18 @@ import { ZoneInfoComponent } from './mir/selections/zones/zone-info/zone-info.co
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    NgxMasonryModule,
     MatToolbarModule,
     MatIconModule,
+    MatCardModule,
     MatButtonModule,
     MatSidenavModule,
-    MatTabsModule
+    MatCheckboxModule,
+    MatInputModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatTabsModule,
+    MatDialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Code/MakeItRain-Ui/src/app/mir/selections/settings/settings.component.ts
+++ Code/MakeItRain-Ui/src/app/mir/selections/settings/settings.component.ts
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./settings.component.scss']
 })
 export class SettingsComponent implements OnInit {
-
+  public editDetails: boolean = false;
+  public editNetwork: boolean = false;
   constructor() { }
 
+  public deviceDetails = {
+    "DeviceName": "MakeItRain",
+    "NodeId": "1"
+  }
+
   ngOnInit(): void {
+  }
+
+  editDetailsFunction(event: string){
+    this.editDetails = false;
+    if(event === 'Save'){
+
+    }else{
+      
+    }
   }
 
 }
