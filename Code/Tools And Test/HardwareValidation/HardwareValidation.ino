158aade1e8e65bde03ccabc79d0ea34f3102804f
Todd J. Lee <tjltm83@gmail.com>
2023-04-17 14:17:46 -0700

    tweaking Test Code/Tools And Test/HardwareValidation/HardwareValidation.ino
    * adding I2C bus communication problem notifications
    * testing some i2c bus speed settings.

--- Code/Tools And Test/HardwareValidation/HardwareValidation.ino
+++ Code/Tools And Test/HardwareValidation/HardwareValidation.ino
@@ -1,6 +1,11 @@
 #include <Wire.h>
 #include <Adafruit_MCP23X08.h>
 
+#define I2C_SDA_PIN 23
+#define I2C_SCL_PIN 22
+Adafruit_MCP23X08 GPIOEX;
+bool GrootToGo = true;
+
 #define VSVoltagePin 33
 #define AUXVoltagePin 32
 #define ResetButton 25
@@ -11,26 +16,31 @@
 #define Zone3Input 36
 #define Zone4Input 39
 
-Adafruit_MCP23X08 GPIOEX;
-
 void setup() {
   Serial.begin(9600);
-  GPIOEX.begin_I2C();
+  //Wire.begin(I2C_SDA_PIN, I2C_SCL_PIN);
+  //Wire.setClock(100000);
+  if (!GPIOEX.begin_I2C()) {
+    Serial.println("Error can not communicate to MCP23008");
+    GrootToGo = false;
+  }
+  else {
+    GPIOEX.pinMode(0, OUTPUT); //Zone 1
+    GPIOEX.pinMode(1, OUTPUT); //Zone 2
+    GPIOEX.pinMode(2, OUTPUT); //Zone 3
+    GPIOEX.pinMode(3, OUTPUT); //Zone 4
+    GPIOEX.pinMode(4, OUTPUT); //Zone 5
+    GPIOEX.pinMode(5, OUTPUT); //Zone 6
+    GPIOEX.pinMode(6, OUTPUT); //Zone 7
+    GPIOEX.pinMode(7, OUTPUT); //Zone 8
+  }
+
   pinMode(LEDOut, OUTPUT);
   pinMode(ResetButton, INPUT);
   pinMode(Zone1Input, INPUT);
   pinMode(Zone2Input, INPUT);
   pinMode(Zone3Input, INPUT);
   pinMode(Zone4Input, INPUT);
-
-  GPIOEX.pinMode(0,OUTPUT); //Zone 1
-  GPIOEX.pinMode(1,OUTPUT); //Zone 2
-  GPIOEX.pinMode(2,OUTPUT); //Zone 3
-  GPIOEX.pinMode(3,OUTPUT); //Zone 4
-  GPIOEX.pinMode(4,OUTPUT); //Zone 5
-  GPIOEX.pinMode(5,OUTPUT); //Zone 6
-  GPIOEX.pinMode(6,OUTPUT); //Zone 7
-  GPIOEX.pinMode(7,OUTPUT); //Zone 8
 }
 
 void loop() {
@@ -39,13 +49,15 @@ void loop() {
   //ReadInputs();
   OutputWalk();
   //OutputInputcheck();
+  //OutputBlink(0,2000);
+  //OutputBlink(7, 2000);
 }
 
 void OutputInputcheck() {
-  GPIOEX.digitalWrite(0,digitalRead(Zone1Input));
-  GPIOEX.digitalWrite(1,digitalRead(Zone2Input));
-  GPIOEX.digitalWrite(2,digitalRead(Zone3Input));
-  GPIOEX.digitalWrite(3,digitalRead(Zone4Input));
+  SetOutput(0, digitalRead(Zone1Input));
+  SetOutput(1, digitalRead(Zone2Input));
+  SetOutput(2, digitalRead(Zone3Input));
+  SetOutput(3, digitalRead(Zone4Input));
 }
 
 void ButtonCheck() {
@@ -82,14 +94,28 @@ void ReadInputs() {
 }
 
 void OutputWalk() {
-  for (int x=0; x < 8; x++){
-    String Message = "Zone " + String(x+1) + " Output ";
-    Serial.println(Message + "High");
-    GPIOEX.digitalWrite(x,HIGH);
-    delay(5000);
-    Serial.println(Message + "LOW");
-    GPIOEX.digitalWrite(x,LOW);
-    delay(5000);
-    
+  for (int x = 0; x < 8; x++) {
+    OutputBlink(x, 2000);
+  }
+}
+
+void OutputBlink(int PortNumber, float Time) {
+
+  String Message = "Zone " + String(PortNumber) + " Output ";
+  Serial.println(Message + "High");
+  SetOutput(PortNumber, HIGH);
+  delay(Time);
+  Serial.println(Message + "LOW");
+  SetOutput(PortNumber, LOW);
+  delay(Time);
+}
+
+
+void SetOutput(int PortNumber, bool State) {
+  if (GrootToGo == true) {
+    GPIOEX.digitalWrite(PortNumber, State);
+  }
+  else {
+    Serial.println("Can Not communicate with MCP23008");
   }
 }
